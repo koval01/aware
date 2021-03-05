31b6fe591c3619dd9f37c0c3f717bc8d59ede892
koval01 <koval.developer@gmail.com>
2021-03-05 22:26:40 +0200

    turn off debug mode

--- qwriter_web/settings.py
+++ qwriter_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY_DJANGO']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 if DEBUG:

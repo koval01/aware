89578f604c2ade46d9cea45a58fc21318fb47207
koval01 <koval.developer@gmail.com>
2021-03-05 21:45:10 +0200

    full update

--- qwriter_web/settings.py
+++ qwriter_web/settings.py
@@ -207,10 +207,10 @@ CSRF_COOKIE_SECURE = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, './my_web/static'),
-# )
+Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, './my_web/static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

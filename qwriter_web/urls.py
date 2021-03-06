b822892377aa21d5df36fbc3709cf5187faff5a6
koval01 <koval.developer@gmail.com>
2021-03-06 20:50:39 +0200

    update urls.py

--- qwriter_web/urls.py
+++ qwriter_web/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_web.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 handler404 = 'my_web.views.error_404'
 handler403 = 'my_web.views.error_403'

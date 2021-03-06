6291b1080e49a446135783091ac30c2ce7f362e0
koval01 <koval.developer@gmail.com>
2021-03-06 20:33:54 +0200

    update settings.py

--- qwriter_web/settings.py
+++ qwriter_web/settings.py
@@ -212,9 +212,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../my_web/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '../my_web/static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

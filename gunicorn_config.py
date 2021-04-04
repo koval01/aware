a7f78079e490277a9eb1fc667838617539f181b8
koval01 <koval.developer@gmail.com>
2021-04-04 18:26:53 +0300

    update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 command = '/home/code/qwriter_web/env/bin/gunicorn'
 pythonpath = '/home/code/qwriter_web/qwriter_web'
 bind = '127.0.0.1:8001'
-workers = 3
+workers = 5
 user = 'admin'
 limit_request_fields = 32000
 limit_request_field_size = 0

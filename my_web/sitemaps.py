6adfef90e40851f3742ca31af52210b472d96b78
koval01 <koval.developer@gmail.com>
2021-04-06 21:03:30 +0300

    experimental update sitemap

--- my_web/sitemaps.py
+++ my_web/sitemaps.py
@@ -4,7 +4,7 @@ from .models import Post, Quote, Facts, Info
 class PostSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.9
-    limit = 1000
+    limit = 100
 
     def items(self):
         return Post.objects.order_by('?')

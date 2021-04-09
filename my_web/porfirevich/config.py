2af4f6c22a39c01ec1a9ba1969cfd473a8e551e8
koval01 <koval.developer@gmail.com>
2021-04-09 20:57:31 +0300

    porfirevich api link update

--- my_web/porfirevich/config.py
+++ my_web/porfirevich/config.py
@@ -1,3 +1,3 @@
-API_URL = 'https://raspy-snowflake-7fad.koval.workers.dev/'
+API_URL = 'https://porfirevich.ru/api/story/?limit=20&offset=0&orderBy=RAND()'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4464.4 Safari/537.36'
 error_check_code = 'the_message_contains_elements_that_are_too_long'

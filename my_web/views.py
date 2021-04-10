5b87b0bb5309e93d5657acff5b100a4175909bed
koval01 <koval.developer@gmail.com>
2021-04-10 17:24:17 +0300

    test update

--- my_web/views.py
+++ my_web/views.py
@@ -195,7 +195,7 @@ def index(request):
     })
 
 
-@ratelimit(key='header:X-Forwarded-For', rate='5/m', block=True)
+@ratelimit(key='header:X-Forwarded-For', rate='5/h', block=True)
 def status(request):
     """
     Status page view

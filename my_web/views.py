0ac8276970fa42599bc0726f6a7347bff918db2a
koval01 <koval.developer@gmail.com>
2021-04-10 17:50:53 +0300

    set image link life time to 900 seconds

--- my_web/views.py
+++ my_web/views.py
@@ -117,7 +117,7 @@ def image_proxy_view(request):
         if img_link_check(link_get):
             token = request.GET['token']
             salt = Fernet(image_proxy_key)
-            token_get = int(salt.decrypt(str.encode(str(token))).decode('utf-8')) + 10
+            token_get = int(salt.decrypt(str.encode(str(token))).decode('utf-8')) + 900
             control_time = round(time())
             if token_get > control_time:
                 try:

fc54a3d149d320408653335ba3f0b760b8c436f2
koval01 <koval.developer@gmail.com>
2021-04-10 16:49:25 +0300

    add AWARE pages to admin panel

--- my_web/admin.py
+++ my_web/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Quote, Facts, Info, Statistic
+from .models import Post, Quote, Facts, Info, Statistic, AWARE_Page
 
 admin.site.register(Post)
 admin.site.register(Quote)
 admin.site.register(Facts)
 admin.site.register(Info)
+admin.site.register(AWARE_Page)
 admin.site.register(Statistic)

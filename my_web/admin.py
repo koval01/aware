ddcb8eec375076e651229a61c7c898da69db2904
koval01 <koval.developer@gmail.com>
2021-04-10 17:07:51 +0300

    api aware update

--- my_web/admin.py
+++ my_web/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin import AdminSite
-from django.utils.translation import ugettext_lazy
 from .models import Post, Quote, Facts, Info, Statistic, AWARE_Page
 
 admin.site.register(Post)
@@ -9,11 +7,4 @@ admin.site.register(Facts)
 admin.site.register(Info)
 admin.site.register(AWARE_Page)
 admin.site.register(Statistic)
-
-class MyAdminSite(AdminSite):
-    site_title = ugettext_lazy('Q-Writer Admin Panel')
-    site_header = ugettext_lazy('Q-Writer Admin Panel')
-    index_title = ugettext_lazy('Q-Writer Admin Panel')
-
-
-admin_site = MyAdminSite()
+admin.site.site_header = 'My administration'

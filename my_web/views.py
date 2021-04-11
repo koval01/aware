bb0f7c492e8ff3ed8297735d905b10535a11041b
koval01 <koval.developer@gmail.com>
2021-04-11 13:02:04 +0300

    test update

--- my_web/views.py
+++ my_web/views.py
@@ -171,7 +171,7 @@ def aware_api(request):
                             break
                     except Exception as e:
                         logger.error(e)
-                    unique_id = str(AWARE_Page.objects.latest('unique_id'))
+                    unique_id = str(AWARE_Page.objects.filter().order_by('-unique_id')[0])
                     print(unique_id)
                     return JsonResponse(
                         {

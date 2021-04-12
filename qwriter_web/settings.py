f8aa740e059158ead15671db2e1cbdace1d7ac6f
koval01 <koval.developer@gmail.com>
2021-04-12 17:41:02 +0300

    gzip middleware

--- qwriter_web/settings.py
+++ qwriter_web/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'qwriter_web.urls'

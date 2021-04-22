b5f21654108efd3007064baa09f3a29a36a2197d
koval01 <koval.developer@gmail.com>
2021-04-22 19:27:20 +0300

    update

--- my_web/calculate.py
+++ my_web/calculate.py
@@ -7,7 +7,9 @@ logger = logging.getLogger(__name__)
 
 def calculator(string) -> str:
     try:
-        s = re.sub(r'[^-+*/()0-9.,\p{Latin}]+', '', string)
+        s = re.sub(r'[^-+*/:\()0-9.,\p{Latin}]+', '', string)
+        s = s.replace(':', '/').replace('\\', '/')
+        s = s.replace('import', '').replace('for', '').replace('while', '')
         return eval(s), s
     except Exception as e:
         logger.error(e)

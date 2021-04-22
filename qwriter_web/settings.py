3210ff9337c74794707508b272338984883c5a84
koval01 <koval.developer@gmail.com>
2021-04-22 19:15:56 +0300

    update

--- qwriter_web/settings.py
+++ qwriter_web/settings.py
@@ -122,17 +122,6 @@ WSGI_APPLICATION = 'qwriter_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'qwriter_database',
-#         'USER': 'root',
-#         'PASSWORD': DB_PASS,
-#         'HOST': DB_HOST,
-#         'PORT': '3306',
-#     },
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',

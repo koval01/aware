6ebee6e37b6c53e139027da88aca25c50649090f
koval01 <koval.developer@gmail.com>
2021-04-23 22:47:32 +0300

    append admin panel link to menu

--- qwriter_web/urls.py
+++ qwriter_web/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin_panel_link"),
     path('', include('my_web.urls')),
 ]
 

70963ce882f4e2dfca888aa7f91afb9dabe38039
koval01 <koval.developer@gmail.com>
2021-04-23 22:49:50 +0300

    url router fix

--- my_web/urls.py
+++ my_web/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.views.decorators.cache import cache_page
 from django.urls import path
@@ -9,6 +10,7 @@ sitemaps_aware_pages = {
 }
 
 urlpatterns = [
+    path('admin/', admin.site.urls, name="admin_panel_link"),
     path('', views.index, name='index_page'),
     path('info', views.info, name='info_page'),
     path('namaz', views.namaz, name='namaz_page'),
--- qwriter_web/urls.py
+++ qwriter_web/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name="admin_panel_link"),
     path('', include('my_web.urls')),
 ]
 

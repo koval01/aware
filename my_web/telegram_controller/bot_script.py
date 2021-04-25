2254cd75a16485d327e82e2917e2a00f7e56b2c3
koval01 <koval.developer@gmail.com>
2021-04-25 22:21:34 +0300

    experimental update

--- my_web/telegram_controller/bot_script.py
+++ my_web/telegram_controller/bot_script.py
@@ -4,10 +4,10 @@ from json import loads
 
 
 def heart(message):
-    print(message)
-    print(loads(message))
-    print(loads(message, object_hook=lambda x: SimpleNamespace(**x)).message)
     message = loads(message, object_hook=lambda x: SimpleNamespace(**x)).message
+    print(message)
+    print(message.text)
+    print(message.chat.id)
     if message.text:
         Message.send_message(message.chat.id, 'Ты отправил мне текст: %s' % message.text)
     else:

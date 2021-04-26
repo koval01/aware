96bcbd1acb42a34c635a53f44530e29d9d90b90c
koval01 <koval.developer@gmail.com>
2021-04-26 22:13:23 +0300

    namaz api update

--- my_web/namaz_api.py
+++ my_web/namaz_api.py
@@ -1,6 +1,7 @@
 from requests import get
 from json import loads
 from .months import convert_short
+from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
@@ -24,6 +25,15 @@ def get_result(city) -> dict:
             logger.error(e)
 
 
+def unix_time_to_day(unix_time) -> int:
+    """
+    Convert unix time to day number
+    :param unix_time: unix time integer
+    :return: day number
+    """
+    return int(datetime.utcfromtimestamp(unix_time).strftime('%d'))
+
+
 def get_namaz_data(city) -> list:
     """
     Prepare and return data
@@ -31,7 +41,9 @@ def get_namaz_data(city) -> list:
     :return: dict
     """
     data = get_result(city)
+    d = int(datetime.now().strftime("%d"))
     return [dict(
         timings=["%s: %s" % (key, value) for key, value in i['timings'].items()],
         time=convert_short(i['date']['readable']),
-    ) for i in data]
+    ) for i in data if d == unix_time_to_day(i['date']['timestamp'])
+                       or d+1 == unix_time_to_day(i['date']['timestamp'])]

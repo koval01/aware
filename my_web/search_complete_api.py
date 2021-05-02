c5fa04f0d2ae1b52272468d0b31061e7dda0f1d5
koval01 <koval.developer@gmail.com>
2021-05-02 22:24:39 +0300

    prepare update v6

--- my_web/search_complete_api.py
+++ my_web/search_complete_api.py
@@ -1,6 +1,7 @@
 from requests import get
 from .covid.config import USER_AGENT
 from bs4 import BeautifulSoup
+from .common_functions import get_random_string
 import logging
 
 logger = logging.getLogger(__name__)
@@ -50,4 +51,4 @@ def get_result_data(question) -> str:
     """
     d = get_result(question)
     r = data_prepare(d)
-    return ''.join(['<li class="aware-recommendation-search-el">%s</li>' % BeautifulSoup(i[0], 'lxml').text.capitalize() for i in r[0]])
+    return ''.join(['<li id="%s" class="aware-recommendation-search-el">%s</li>' % (get_random_string(), BeautifulSoup(i[0], 'lxml').text.capitalize()) for i in r[0]])

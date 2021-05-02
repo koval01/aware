e4fb45c407bf8827efa0ecfeb2de302bd90d3939
koval01 <koval.developer@gmail.com>
2021-05-02 13:48:22 +0300

    news update

--- my_web/text_to_image_api.py
+++ my_web/text_to_image_api.py
@@ -39,7 +39,7 @@ def image_edit(image, text, author) -> bytes:
     img = Image.open(BytesIO(image))
 
     blured_image = img.filter(ImageFilter.GaussianBlur(15))
-    text = '-\n'.join(text_formatting(text))
+    text = '\n'.join(text_formatting(text))
     author = '— %s' % author
     base_text = ImageFont.truetype(font_root_Roboto, 46)
     author_font = ImageFont.truetype(font_root_Roboto, 38)

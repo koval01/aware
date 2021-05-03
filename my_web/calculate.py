ef6fa9ec34de065c69428fe64fa56d987d1cdeda
koval01 <koval.developer@gmail.com>
2021-05-03 21:19:37 +0300

    calculate.py update

--- my_web/calculate.py
+++ my_web/calculate.py
@@ -7,11 +7,10 @@ logger = logging.getLogger(__name__)
 
 def calculator(string) -> str:
     try:
-        s = string.replace('×', '*').replace('π', 'pi').replace('п', 'pi')
+        s = string.lower().replace('×', '*').replace('π', 'pi').replace('п', 'pi')
         s = re.sub(r'[^-+*/:\()0-9.,\s\p{Latin}]+', '', s)
         s = s.replace(':', '/').replace('\\', '/')
         s = s.replace('import', '').replace('for', '').replace('while', '')
-        s = s.lower()
         return eval(s), s
     except Exception as e:
         logger.error(e)

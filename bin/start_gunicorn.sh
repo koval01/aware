781d7e893c92d1e48c16549bddc2c5d725d62cef
koval01 <koval.developer@gmail.com>
2021-05-04 22:35:57 +0300

    update view

--- bin/start_gunicorn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /home/code/qwriter_web/env/bin/activate
-# source /home/www/code/project/env/bin/postactivate
-exec gunicorn  -c "/home/code/qwriter_web/gunicorn_config.py" qwriter_web.wsgi

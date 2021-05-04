71b76004eaf7555cbac8129c3247dcbc8e221104
koval01 <koval.developer@gmail.com>
2021-05-04 23:04:33 +0300

    update view

--- my_web/views.py
+++ my_web/views.py
@@ -269,7 +269,7 @@ def search_suggestions_get(request):
     """
     try:
         q = request.GET['q']
-        if q and len(q) < 500:
+        if q and len(q) < 200:
             return JsonResponse({"data": search_complete(q)})
     except Exception as e:
         logger.error(e)

71eeb20cd4a6f5f9602e9b64e443587cc64c622a
koval01 <koval.developer@gmail.com>
2021-05-09 21:13:09 +0300

    Experimental update

--- my_web/views.py
+++ my_web/views.py
@@ -303,7 +303,8 @@ def get_ad(request):
                 for i in all_data:
                     if i.i_chance >= randint(1, 100) \
                             and randint(1, 6) > randint(1, 6) \
-                            and round(time()) < round(i.i_time_active.timestamp()):
+                            and round(time()) < round(i.i_time_active.timestamp()) \
+                            and i.i_active == 'yes':
                         done_get = True
                         i.i_views += 1  # Add one view
                         return JsonResponse(

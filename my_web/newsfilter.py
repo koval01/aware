513e7cdc8ff0607c35e935b1879f5e87b1e1ec89
koval01 <koval.developer@gmail.com>
2021-05-10 20:38:24 +0300

    update newsfilter.py

--- my_web/newsfilter.py
+++ my_web/newsfilter.py
@@ -1,16 +1,19 @@
+from bs4 import BeautifulSoup
 import logging
 
 
-# Module for filtering the context of the news feed
-
 def text_news_filter(string):
-    # Remove the protocol from the links
+    """
+    Remove other elements from text
+    :param string: string
+    :return: edited string
+    """
     string = str(string).replace('https://', '').replace('http://', '')
     # Remove the markup.
-    for i in range(5):
-        string = string.replace('&raquo;', '').replace('&laquo;', '').replace('&nbsp;', '')
-        string = string.replace('\n', '').replace('\r', '').replace('\t', '')
+    # string = string.replace('&raquo;', '').replace('&laquo;', '').replace('&nbsp;', '').replace(
+    #         '&mdash;', '')
+    # string = string.replace('\n', '').replace('\r', '').replace('\t', '')
 
     logging.debug('News feed successfully filtered.')
 
-    return string
+    return BeautifulSoup(string, 'lxml').text

68c9e5bfcf12d22df23b2498b577a67796a4ef53
koval01 <koval.developer@gmail.com>
2021-05-10 16:10:01 +0300

    update admin.py

--- my_web/admin.py
+++ my_web/admin.py
@@ -4,3 +4,8 @@ from .models import Info, AWARE_Page
 admin.site.register(Info)
 admin.site.register(AWARE_Page)
 admin.site.site_header = 'Aware Admin Panel'
+
+
+class InfoAdmin(admin.ModelAdmin):
+    readonly_fields = ('i_views',)
+    search_fields = ('i_text',)

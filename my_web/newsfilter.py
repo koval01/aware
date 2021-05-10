b5379b1c377f40a993d62161d04bf471d7e885d7
koval01 <koval.developer@gmail.com>
2021-05-10 20:39:26 +0300

    update newsfilter.py

--- my_web/newsfilter.py
+++ my_web/newsfilter.py
@@ -9,11 +9,5 @@ def text_news_filter(string):
     :return: edited string
     """
     string = str(string).replace('https://', '').replace('http://', '')
-    # Remove the markup.
-    # string = string.replace('&raquo;', '').replace('&laquo;', '').replace('&nbsp;', '').replace(
-    #         '&mdash;', '')
-    # string = string.replace('\n', '').replace('\r', '').replace('\t', '')
-
     logging.debug('News feed successfully filtered.')
-
     return BeautifulSoup(string, 'lxml').text

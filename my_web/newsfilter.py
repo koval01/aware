57fbde6221e41f334ba0d99c755f200a7d6397e9
koval01 <koval.developer@gmail.com>
2021-05-10 20:31:59 +0300

    update newsfilter.py

--- my_web/newsfilter.py
+++ my_web/newsfilter.py
@@ -8,5 +8,6 @@ def text_news_filter(string):
     string = str(string).replace('https://', '').replace('http://', '')
     # Remove the markup.
     string = string.replace('&raquo;', '').replace('&laquo;', '').replace('&nbsp;', '')
+    string = string.replace('\n', '').replace('\r', '').replace('\t', '')
     logging.debug('News feed successfully filtered.')
     return string

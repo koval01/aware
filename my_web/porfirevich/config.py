f38852e4eb7220ac8c26cc8cb6191d67070687bc
Koval <61350874+koval01@users.noreply.github.com>
2021-07-21 22:35:23 +0300

    Delete my_web/porfirevich directory

--- my_web/porfirevich/config.py
+++
@@ -1,3 +0,0 @@
-API_URL = 'https://porfirevich.ru/api/story/?limit=20&offset=0&orderBy=RAND()'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4464.4 Safari/537.36'
-error_check_code = 'the_message_contains_elements_that_are_too_long'

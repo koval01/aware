a143e83bfddd40f7034c1ad1ca8de5d32f19e74b
yaroslav.koval <koval.developer@gmail.com>
2021-07-22 20:28:00 +0300

    refactoring (hotfix)

--- aware_web/wsgi.py
+++ aware_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qwriter_web project.
+WSGI config for aware_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qwriter_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aware_web.settings')
 
 application = get_wsgi_application()

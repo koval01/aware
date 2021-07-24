f183c1610e8cb3f335cc716488370ed007605ea5
yaroslav.koval <koval.developer@gmail.com>
2021-07-24 14:52:33 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aware_site',
+    version='1.0',
+    packages=['awse', 'awse.migrations', 'awse.news_utils', 'awse.search_utils', 'aware_web'],
+    url='https://www.awse.us/',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Koval Yaroslav',
+    author_email='koval@q-writer.com',
+    description='AWARE - Search engine'
+)

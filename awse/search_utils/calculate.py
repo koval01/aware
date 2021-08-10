6efa52fd6c508da5a7826b07628cf150096f570f
Koval Yaroslav <koval@q-writer.com>
2021-08-10 13:19:00 +0300

    Update calculate.py

--- awse/search_utils/calculate.py
+++ awse/search_utils/calculate.py
@@ -9,7 +9,7 @@ def calculator(string) -> str:
     try:
         s = string.lower().replace('×', '*').replace('π', 'pi').replace('п', 'pi')
         s = re.sub(r'[^-+*/:\()0-9.,\s\p{Latin}]+', '', s)
-        s = s.replace(':', '/').replace('\\', '/')
+        s = s.replace(':', '/').replace('\\', '/').replace(",", ".")
         s = s.replace('import', '').replace('for', '').replace('while', '')
         if any(word in s for word in ['+', '-', '*', '/']):
             return eval(s), s

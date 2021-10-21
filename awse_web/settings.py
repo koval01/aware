109cc5df1231357c8d8c6ca6b007f89138c2d6f4
yaroslav.koval <ksvarog2004@gmail.com>
2021-10-21 21:03:53 +0300

    update

--- awse_web/settings.py
+++ awse_web/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.environ['SECRET_KEY_DJANGO']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOCAL_DATABASE = False
 CACHE_IN_MEMORY_SERVER = True

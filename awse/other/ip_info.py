f815ec580a70134e2fae1e434fbd1d7e35bf1786
yaroslav.koval <ksvarog2004@gmail.com>
2021-10-22 19:10:31 +0300

    simple update

--- awse/other/ip_info.py
+++ awse/other/ip_info.py
@@ -9,3 +9,4 @@ def get_data(ipaddress) -> dict:
         return get("http://ip-api.com/json/%s" % ipaddress).json()["countryCode"].lower()
     except Exception as e:
         logger.error("User country code get error - %s" % e)
+        return "us"

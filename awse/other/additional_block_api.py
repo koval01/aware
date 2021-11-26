4388d8f00f5cfa35dd637668e4196fee8b24bd18
yaroslav.koval <ksvarog2004@gmail.com>
2021-11-26 21:47:44 +0200

    update

--- awse/other/additional_block_api.py
+++ awse/other/additional_block_api.py
@@ -1,74 +1,4 @@
 from requests import get
-from pydantic import BaseModel
-
-
-class InfoboxContentModel(BaseModel):
-    data_type: str = None
-    label: str = None
-    value: str = None
-    wiki_order: str or int = None
-
-
-class InfoboxMetaModel(BaseModel):
-    data_type: str = None
-    label: str = None
-    value: str = None
-
-
-class InfoboxModel(BaseModel):
-    content: [InfoboxContentModel] = None
-    meta: [InfoboxMetaModel] = None
-
-
-class IconModel(BaseModel):
-    Height: str or int = None
-    URL: str = None
-    Width: str or int = None
-
-
-class AdditionalDataModel(BaseModel):
-    FirstURL: str = None
-    Icon: IconModel = None
-    Result: str = None
-    Text: str = None
-
-
-class MetaModel(BaseModel):
-    description: str = None
-    id: str = None
-    name: str = None
-    src_domain: str = None
-
-
-class ResultModel(BaseModel):
-    Abstract: str = None
-    AbstractSource: str = None
-    AbstractText: str = None
-    AbstractURL: str = None
-
-    Answer: str = None
-    AnswerType: str = None
-
-    Definition: str = None
-    DefinitionSource: str = None
-    DefinitionURL: str = None
-
-    Entity: str = None
-    Heading: str = None
-
-    Image: str = None
-    ImageHeight: int = 0
-    ImageIsLogo: bool = False
-    ImageWidth: int = 0
-
-    Infobox: InfoboxModel = None
-    Redirect: str = None
-
-    RelatedTopics: [AdditionalDataModel] = None
-    Results: [AdditionalDataModel] = None
-
-    Type: str = None
-    meta: MetaModel = None
 
 
 class AdditionalBlock:
@@ -86,12 +16,7 @@ class AdditionalBlock:
         if response.status_code >= 200 < 300:
             return response.json()
 
-    def collected(self) -> dict or None:
-        data = self.get_json()
-        if data["RelatedTopics"] and data["Heading"]:
-            return ResultModel(data)
-
     def get(self) -> dict or None:
-        data = self.collected()
-        if data and data.AbstractText:
+        data = self.get_json()
+        if data["RelatedTopics"] and data["Heading"] and data["AbstractText"]:
             return data
